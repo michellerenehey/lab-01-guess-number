ffb12447968653f4c11f550bb2baa949efc63c03
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-04 16:56:20 -0700

    added CSS styling and complete reset button

--- app.js
+++ app.js
@@ -6,33 +6,38 @@ const userMessage = document.getElementById('user-message');
 const resetButton = document.getElementById('reset-button'); 
 
 // set state 
-let numGuesses = 0; 
+let numGuesses = 4; 
 let randomNum = (Math.floor(Math.random() * 20)+1);
+console.log(randomNum, "original random number");
 
 
 // event listener
 submitButton.addEventListener('click', () => {
-  numGuesses ++; 
+  numGuesses --; 
   timesGuessed.textContent = numGuesses; 
   const userGuess = Number(userInput.value); 
   let result; 
   if (userGuess === randomNum){
-    result = "You win"
-  } else if (numGuesses === 4) {
-    result = "You're out of guesses"; 
+    result = "You win!!!"
+  } else if (numGuesses === 0) {
+    result = "You're out of guesses :( :( :("; 
     submitButton.style.display = "none";
     resetButton.style.display = "block"; 
   } else if (userGuess > randomNum) {
-    result = "You guessed too high"
+    result = "You guessed too high!"
   } else {
-    result = "You guessed too low"
+    result = "You guessed too low!"
   }; 
   userMessage.textContent = result;
   });
   
 resetButton.addEventListener('click', () => {
-  numGuesses = 0; 
-  randomNum = "", 
+  numGuesses = 4; 
+  randomNum = (Math.floor(Math.random() * 20)+1);
+  console.log(randomNum, "new random number");
+  userInput.value = ""; 
+  userMessage.textContent = ""; 
+  timesGuessed.textContent = 4;
   submitButton.style.display = "block"; 
   resetButton.style.display = "none"; 
 })
