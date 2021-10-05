787d60b6046763477602524ba985caafa8b3d3ba
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-05 07:38:21 -0700

    fixed ESLint problems

--- app.js
+++ app.js
@@ -8,39 +8,39 @@ const guessesRemaining = document.getElementById('guesses-remaining');
 
 // set state 
 let numGuesses = 4; 
-let randomNum = (Math.floor(Math.random() * 20)+1);
-console.log(randomNum, "first random number"); 
+let randomNum = (Math.floor(Math.random() * 20) + 1);
+console.log(randomNum, 'first random number'); 
 
 // event listener
 submitButton.addEventListener('click', () => {
-  numGuesses --; 
-  timesGuessed.textContent = numGuesses; 
-  const userGuess = Number(userInput.value); 
-  let result; 
-  if (userGuess === randomNum){
-    result = "You win!!!"
-    // guessesRemaining.textContent = "";
-    // submitButton.style.display = "none";
-    // resetButton.style.display = "block"; 
-  } else if (numGuesses === 0) {
-    result = "You're out of guesses :( :( :("; 
-    submitButton.style.display = "none";
-    resetButton.style.display = "block"; 
-  } else if (userGuess > randomNum) {
-    result = "You guessed too high!"
-  } else {
-    result = "You guessed too low!"
-  }; 
-  userMessage.textContent = result;
-  });
+    numGuesses --; 
+    timesGuessed.textContent = numGuesses; 
+    const userGuess = Number(userInput.value); 
+    let result; 
+    if (userGuess === randomNum){
+        result = 'You win!!!'; 
+        // guessesRemaining.textContent = "";
+        // submitButton.style.display = "none";
+        // resetButton.style.display = "block"; 
+    } else if (numGuesses === 0) {
+        result = "You're out of guesses :( :( :("; 
+        submitButton.style.display = 'none';
+        resetButton.style.display = 'block'; 
+    } else if (userGuess > randomNum) {
+        result = 'You guessed too high!'; 
+    } else {
+        result = 'You guessed too low!';
+    } 
+    userMessage.textContent = result;
+});
   
-  resetButton.addEventListener('click', () => {
+resetButton.addEventListener('click', () => {
     numGuesses = 4; 
-    randomNum = (Math.floor(Math.random() * 20)+1);
-    console.log(randomNum, "new random number");
-    userInput.value = ""; 
-    userMessage.textContent = ""; 
+    randomNum = (Math.floor(Math.random() * 20) + 1);
+    console.log(randomNum, 'new random number');
+    userInput.value = ''; 
+    userMessage.textContent = ''; 
     timesGuessed.textContent = 4;
-    submitButton.style.display = "block"; 
-    resetButton.style.display = "none"; 
-  });
+    submitButton.style.display = 'block'; 
+    resetButton.style.display = 'none'; 
+});
