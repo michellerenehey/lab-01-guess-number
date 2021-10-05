3081ce58cfb74163855feacbc05eed67eec175fd
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-05 10:51:54 -0700

    added cypress testing

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -5,8 +5,15 @@ describe('Example Test', ()=>{
     });
 
     describe('HTML elements', ()=>{   
-        it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+        it('contains user-input', ()=>{
+            cy.get('#user-input').should('be.visible');
+        });
+    });
+
+    describe('user interaction', ()=>{   
+        it('should show taht the button was clicked', ()=>{
+            cy.get('#submit-button').click();
+            cy.get('#user-message').should('be.visible');
         });
     });
 });
