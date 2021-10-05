6fdb614966c5139710dbaf6187412bc91fb13617
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-04 17:21:37 -0700

    set up TDD of compareNumbers function

---
+++ compareNumbers.js
@@ -0,0 +1,8 @@
+export function compareNumbers (guess, correctNumber); 
+    if (guess === correctNumber) {
+        return 0;
+    } else if (guess < correctNumber) {
+        return -1;
+    } else (guess > correctNumber) {
+        return 1;ß
+    }
--- test/TDD.compareNumbers.test.js
+++ test/TDD.compareNumbers.test.js
@@ -3,14 +3,16 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('compare numbers function (5,5)', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const guess = 5; 
+    const correctNumber = 5; 
+    const expected = 0;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = compareNumbers(actual, expected);
 
     //Expect
     // Make assertions about what is expected versus the actual result
