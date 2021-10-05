53fa0399abab8e3f094fa6c26d69b4f349d2f9d4
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-04 20:09:41 -0700

    TDD tests working, need more testing

--- compareNumbers.js
+++ compareNumbers.js
@@ -1,8 +1,8 @@
-export function compareNumbers (guess, correctNumber); 
+export function compareNumbers (guess, correctNumber){
     if (guess === correctNumber) {
         return 0;
     } else if (guess < correctNumber) {
         return -1;
-    } else (guess > correctNumber) {
-        return 1;ß
-    }
+    } else if (guess > correctNumber) {
+        return 1;
+    }}
