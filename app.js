e1ab1f080e85805c9a40f82d4be43707ffec9d8e
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-04 18:31:16 -0700

    unsucessful removal of guesses remaining, submit button, etc, when win

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const submitButton = document.getElementById('submit-button');
 const timesGuessed = document.getElementById('times-guessed'); 
 const userMessage = document.getElementById('user-message'); 
 const resetButton = document.getElementById('reset-button'); 
+const guessesRemaining = document.getElementById('guesses-remaining'); 
 
 // set state 
 let numGuesses = 4; 
 let randomNum = (Math.floor(Math.random() * 20)+1);
-console.log(randomNum, "original random number");
-
+console.log(randomNum, "first random number"); 
 
 // event listener
 submitButton.addEventListener('click', () => {
@@ -19,6 +19,9 @@ submitButton.addEventListener('click', () => {
   let result; 
   if (userGuess === randomNum){
     result = "You win!!!"
+    // guessesRemaining.textContent = "";
+    // submitButton.style.display = "none";
+    // resetButton.style.display = "block"; 
   } else if (numGuesses === 0) {
     result = "You're out of guesses :( :( :("; 
     submitButton.style.display = "none";
@@ -31,13 +34,13 @@ submitButton.addEventListener('click', () => {
   userMessage.textContent = result;
   });
   
-resetButton.addEventListener('click', () => {
-  numGuesses = 4; 
-  randomNum = (Math.floor(Math.random() * 20)+1);
-  console.log(randomNum, "new random number");
-  userInput.value = ""; 
-  userMessage.textContent = ""; 
-  timesGuessed.textContent = 4;
-  submitButton.style.display = "block"; 
-  resetButton.style.display = "none"; 
-})
+  resetButton.addEventListener('click', () => {
+    numGuesses = 4; 
+    randomNum = (Math.floor(Math.random() * 20)+1);
+    console.log(randomNum, "new random number");
+    userInput.value = ""; 
+    userMessage.textContent = ""; 
+    timesGuessed.textContent = 4;
+    submitButton.style.display = "block"; 
+    resetButton.style.display = "none"; 
+  });
