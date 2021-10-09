e3f6c8eb4a3923ed7bd761b76e8b60fc1eb57233
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-08 17:15:07 -0700

    added start over button

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const timesGuessed = document.getElementById('times-guessed');
 const userMessage = document.getElementById('user-message'); 
 const resetButton = document.getElementById('reset-button'); 
 const guessesRemaining = document.getElementById('guesses-remaining'); 
+const rightCount = document.getElementById('correct-guess');
+const wrongCount = document.getElementById('incorrect-guess');
+const startOverButton = document.getElementById('new-game');
 
 // set state 
 let numGuesses = 4; 
 let randomNum = (Math.floor(Math.random() * 20) + 1);
 console.log(randomNum, 'first random number'); 
+let correct = 0;
+let incorrect = 0;
 
 
 // import function
@@ -24,6 +29,8 @@ submitButton.addEventListener('click', () => {
     
     if (compareNumbers(userGuess, randomNum) === 0) {
         result = 'You win!!!'; 
+        correct++;
+        rightCount.textContent = correct;
         guessesRemaining.textContent = '';
         submitButton.style.display = 'none';
         resetButton.style.display = 'block';
@@ -37,11 +44,15 @@ submitButton.addEventListener('click', () => {
     }
 
     if (compareNumbers(userGuess, randomNum) === 1) {
-        result = 'You guessed too high!';         
+        result = 'You guessed too high!'; 
+        incorrect++;     
+        wrongCount.textContent = incorrect;   
     }
 
     if (compareNumbers(userGuess, randomNum) === -1) {
         result = 'You guessed too low!';
+        incorrect++;
+        wrongCount.textContent = incorrect;   
     }
     
     userMessage.textContent = result;
@@ -54,6 +65,19 @@ resetButton.addEventListener('click', () => {
     console.log(randomNum, 'new random number');
     userInput.value = ''; 
     userMessage.textContent = ''; 
+    guessesRemaining.textContent = `You have ${numGuesses} guesses remaining.`;
+    submitButton.style.display = 'block'; 
+    resetButton.style.display = 'none'; 
+});
+
+startOverButton.addEventListener('click', ()=>{
+    numGuesses = 4;
+    incorrect = 0;
+    correct = 0;
+    rightCount.textContent = '0';
+    wrongCount.textContent= '0';
+    userInput.value = ''; 
+    userMessage.textContent = '';
     guessesRemaining.textContent = `You have ${numGuesses} guesses remaining.`;
     submitButton.style.display = 'block'; 
     resetButton.style.display = 'none'; 
